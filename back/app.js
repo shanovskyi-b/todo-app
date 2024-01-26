211a792f5dc0f441e00df1881ce0f6eeb3c837c7
shanovskyi-b <shanovskyi.b@gmail.com>
2024-01-26 09:34:11 +0200

    fix: update CORS

--- back/app.js
+++ back/app.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.json({ taskLists: [{ id: 'list-01', title: 'List 1' }, { id: 'list-02', title: 'List 2' }] })
+app.use(cors());
+
+app.get('/list/', (req, res) => {
+  res.statusCode = 200;
+  res.json({ lists: [{ id: 'list-01', title: 'List 1' }, { id: 'list-02', title: 'List 2' }] });
 });
 
 app.listen(port, () => {
