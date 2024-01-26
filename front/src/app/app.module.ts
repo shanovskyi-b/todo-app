35db364d4eceac6f8518703b0a9ada383d4ff2c7
JumJun <dremovigor919@gmail.com>
2024-01-26 17:17:31 +0200

    test-change

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StyleguideComponent } from './styleguide/styleguide.component';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StyleguideComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/src/app/styleguide/styleguide.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-styleguide',
+  templateUrl: './styleguide.component.html',
+  styleUrl: './styleguide.component.scss'
+})
+export class StyleguideComponent {
+
+}
