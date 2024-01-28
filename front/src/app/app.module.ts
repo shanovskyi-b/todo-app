8aa4cd6b594f31a03410f66fcfa16b8758e68505
JumJun <dremovigor919@gmail.com>
2024-01-28 17:38:18 +0200

    style update

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StyleguideComponent } from './styleguide/styleguide.component';
-import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleguideComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
