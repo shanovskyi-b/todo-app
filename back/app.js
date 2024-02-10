99049be5f445fb3002b6a1bef30da339a046e3b5
shanovskyi-b <shanovskyi.b@gmail.com>
2024-02-10 13:43:27 +0200

    feat: add get list by id

--- back/app.js
+++ back/app.js
@@ -5,9 +5,41 @@ const port = 3000;
 
 app.use(cors());
 
+const ALL_LISTS = [
+  { id: 'list-01', title: 'Home' }, 
+  { id: 'list-02', title: 'Work' }
+];
+
+const TASKS = {
+  'list-01': [
+    { id: 'task-01', title: 'Buy bananas' },
+    { id: 'task-02', title: 'Vacuum' },
+  ],
+  'list-02': [
+    { id: 'task-03', title: 'Check Friday\'s email regarding prod issue' },
+    { id: 'task-04', title: 'Write to Maria to heck her appoinment' },
+  ],
+}
+
 app.get('/list/', (req, res) => {
   res.statusCode = 200;
-  res.json({ lists: [{ id: 'list-01', title: 'List 1' }, { id: 'list-02', title: 'List 2' }] });
+  res.json({ lists: ALL_LISTS });
+});
+
+app.get('/list/:id', (req, res) => {
+  const listId = req.params.id;
+  if (!ALL_LISTS.find(list => list.id === listId)) {
+    res.statusCode = 400;
+  
+    res.json({ error: `No list found by id: ${listId}` });
+    return;
+  }
+
+  const listTasks = TASKS[listId] ?? [];
+
+  res.statusCode = 200;
+  
+  res.json({ tasks: listTasks });
 });
 
 app.listen(port, () => {
